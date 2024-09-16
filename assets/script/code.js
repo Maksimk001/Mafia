4a5f5814024af4bb49df88580ea9ae38ed30e186
Maksimk001 <newmaks001@mail.ru>
2024-09-16 15:08:11 +0500

    3

--- assets/script/code.js
+++ assets/script/code.js
@@ -1,7 +1,8 @@
 let settings = document.querySelector('.link')
 let sname = document.querySelectorAll('.block p') 
 let button = document.querySelectorAll('.kill')
-
+let inputleft = document.querySelectorAll('div.block input.left')
+let inputright = document.querySelectorAll('div.block input.right')
 
 settings.onclick = function() {
     setting()
@@ -22,6 +23,8 @@ for (let i = 0; i < button.length; i++) {
     function killme() {
             button[i].style.backgroundColor = 'red'
             sname[i].style.color = 'red'
+            inputleft[i].style.border = '1px solid red'
+            inputright[i].style.border = '1px solid red'
     }
 }
 
