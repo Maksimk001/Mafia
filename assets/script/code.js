e4eb5f37a2268bc1c94a8781a88f01d360b0a20c
Maksimk001 <newmaks001@mail.ru>
2024-09-17 18:59:50 +0500

    4

--- assets/script/code.js
+++ assets/script/code.js
@@ -1,19 +1,7 @@
-let settings = document.querySelector('.link')
 let sname = document.querySelectorAll('.block p') 
 let button = document.querySelectorAll('.kill')
 let inputleft = document.querySelectorAll('div.block input.left')
 let inputright = document.querySelectorAll('div.block input.right')
-
-settings.onclick = function() {
-    setting()
-}
-function setting() {
-    window.location.href = '/assets/pages/setening.html';
-}
-
-
-
-
 
 
 for (let i = 0; i < button.length; i++) {
@@ -28,3 +16,60 @@ for (let i = 0; i < button.length; i++) {
     }
 }
 
+
+
+// Настройки
+let opclsetting = document.querySelector('button.link')
+let setting = document.querySelector('div.setting')
+opclsetting.onclick = function() {
+    openclose()
+}
+function openclose() {
+    setting.classList.toggle('close')
+}
+
+
+
+let minus = document.querySelectorAll('button.minus')
+let plus = document.querySelectorAll('button.plus')
+let rolskol = document.querySelectorAll('p.kol')
+
+for (let i = 0; i < minus.length; i++) {
+    minus[i].onclick = function() {
+        minusrols();
+
+        function minusrols() {
+            if (rolskol[i].textContent != 0)
+            rolskol[i].textContent = rolskol[i].textContent - 1
+        }
+    }
+}
+for (let i = 0; i < plus.length; i++) {
+    plus[i].onclick = function() {
+        plusrols();
+
+        function plusrols() {
+            rolskol[i].textContent =+ rolskol[i].textContent + 1
+        }
+    }
+}
+
+
+let savebutton = document.querySelector('button.save')
+let savebuttonstat = true
+savebutton.onclick = function() {
+    saveroles()
+}
+function saveroles() {
+    savebutton.style.backgroundColor = 'gray'
+    
+    let rolsarr = {
+        'Killer': rolskol[0].textContent,
+        'Mafia': rolskol[1].textContent,
+        'Doctor': rolskol[2].textContent,
+        'Shreif': rolskol[3].textContent,
+        'Love': rolskol[4].textContent,
+        'funny': rolskol[5].textContent
+    }
+    console.log(rolsarr);
+}
