f4128e743a9ddcf66b0ec685a84ffc15c03917b8
Maksimk001 <newmaks001@mail.ru>
2024-09-17 19:52:40 +0500

    5

--- assets/script/code.js
+++ assets/script/code.js
@@ -1,26 +1,13 @@
-let sname = document.querySelectorAll('.block p') 
-let button = document.querySelectorAll('.kill')
-let inputleft = document.querySelectorAll('div.block input.left')
-let inputright = document.querySelectorAll('div.block input.right')
-
-
-for (let i = 0; i < button.length; i++) {
-    button[i].onclick = function() {
-        killme()
-    }
-    function killme() {
-            button[i].style.backgroundColor = 'red'
-            sname[i].style.color = 'red'
-            inputleft[i].style.border = '1px solid red'
-            inputright[i].style.border = '1px solid red'
-    }
-}
-
-
-
 // Настройки
+
 let opclsetting = document.querySelector('button.link')
 let setting = document.querySelector('div.setting')
+let minus = document.querySelectorAll('button.minus')
+let plus = document.querySelectorAll('button.plus')
+let rolskol = document.querySelectorAll('p.kol')
+let savebutton = document.querySelector('button.save')
+let savebuttonstat = true
+
 opclsetting.onclick = function() {
     openclose()
 }
@@ -30,9 +17,7 @@ function openclose() {
 
 
 
-let minus = document.querySelectorAll('button.minus')
-let plus = document.querySelectorAll('button.plus')
-let rolskol = document.querySelectorAll('p.kol')
+
 
 for (let i = 0; i < minus.length; i++) {
     minus[i].onclick = function() {
@@ -55,8 +40,7 @@ for (let i = 0; i < plus.length; i++) {
 }
 
 
-let savebutton = document.querySelector('button.save')
-let savebuttonstat = true
+
 savebutton.onclick = function() {
     saveroles()
 }
@@ -72,4 +56,105 @@ function saveroles() {
         'funny': rolskol[5].textContent
     }
     console.log(rolsarr);
+    addRoles(rolsarr)
+}
+
+function addRoles(rolsarr) {
+    addKiller(rolsarr)
+    addMafia(rolsarr)
+    addDoctor(rolsarr)
+    addShreif(rolsarr)
+    addLove(rolsarr)
+    addLive(rolsarr)
+}
+function addKiller(rolsarr) {
+
+    for (let i = 0; i < rolsarr.Killer; i++) {
+        document.querySelector("div.cart").innerHTML +=`
+        <div class="block" id="killer">
+            <input type="text" class="left">
+            <p>Маньяк</p>
+            <input type="text" class="right">
+            <button class="kill"></button>
+        </div>`;
+    }
+}
+function addMafia(rolsarr) {
+    for (let i = 0; i < rolsarr.Mafia; i++) {
+        document.querySelector("div.cart").innerHTML +=`
+        <div class="block" id="Mafia">
+            <input type="text" class="left">
+            <p>Мафия</p>
+            <input type="text" class="right">
+            <button class="kill"></button>
+        </div>`;
+    }
+}
+function addDoctor(rolsarr) {
+    for (let i = 0; i < rolsarr.Doctor; i++) {
+        document.querySelector("div.cart").innerHTML +=`
+        <div class="block" id="Doctor">
+            <input type="text" class="left">
+            <p>Доктор</p>
+            <input type="text" class="right">
+            <button class="kill"></button>
+        </div>`;
+    }
+}
+function addShreif(rolsarr) {
+    for (let i = 0; i < rolsarr.Shreif; i++) {
+        document.querySelector("div.cart").innerHTML +=`
+        <div class="block" id="Shreif">
+            <input type="text" class="left">
+            <p>Комисар</p>
+            <input type="text" class="right">
+            <button class="kill"></button>
+        </div>`;
+    }
+}
+function addLove(rolsarr) {
+    for (let i = 0; i < rolsarr.Love; i++) {
+        document.querySelector("div.cart").innerHTML +=`
+        <div class="block" id="Love">
+            <input type="text" class="left">
+            <p>Любовница</p>
+            <input type="text" class="right">
+            <button class="kill"></button>
+        </div>`;
+    }
+}
+function addLive(rolsarr) {
+    for (let i = 0; i < rolsarr.funny; i++) {
+        document.querySelector("div.cart").innerHTML +=`
+        <div class="block" id="Live">
+            <input type="text" class="left">
+            <p>Обыватель</p>
+            <input type="text" class="right">
+            <button class="kill"></button>
+        </div>`;
+    }
+
+    initializing()
+}
+
+
+function initializing() {
+
+let sname = document.querySelectorAll('.block p') 
+let button = document.querySelectorAll('button.kill')
+let inputleft = document.querySelectorAll('div.block input.left')
+let inputright = document.querySelectorAll('div.block input.right')
+
+console.log(button[0]);
+for (let i = 0; i < button.length; i++) {
+    button[i].onclick = function() {
+        killme()
+    }
+    function killme() {
+            button[i].style.backgroundColor = 'red'
+            sname[i].style.color = 'red'
+            inputleft[i].style.border = '1px solid red'
+            inputright[i].style.border = '1px solid red'
+    }
+}
 }
